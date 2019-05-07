0bafae084a2887266ebd9035aed44d61079521f3
chiknahuikoatl <jalrod@ciencias.unam.mx>
2019-05-07 18:54:44 -0500

    Clase Fecha con funcionalidad mínima necesaria terminada.

--- Tarea05/src/Fecha.java
+++ Tarea05/src/Fecha.java
@@ -2,7 +2,7 @@
  * Clase que simula fecha.
  */
 
-public Class Fecha{
+public class Fecha{
 
     //Elementos de la fecha.
     private int dia;
@@ -22,15 +22,49 @@ public Class Fecha{
     }
 
     public Fecha siguienteDia(Fecha fecha){
-
+        switch(fecha.getMes()){
+            case 1: case 3: case 5: case 7:
+            case 8: case 10:
+                if(fecha.getDia() == 31){
+                    fecha = siguienteMes(fecha);
+                    fecha.setDia(1);
+                }else{
+                    fecha.setDia(fecha.getDia() + 1);
+                }
+                return fecha;
+            case 4: case 6: case 9: case 11:
+                if(fecha.getDia() == 30){
+                    fecha = siguienteMes(fecha);
+                    fecha.setDia(1);
+                }else{
+                    fecha.setDia(fecha.getDia() + 1);
+                }
+                return fecha;
+            case 12:
+                if(fecha.getDia() == 31){
+                    fecha = siguienteMes(fecha);
+                    fecha.setDia(1);
+                    fecha.setAnio(fecha.getAnio() + 1);
+                }else{
+                    fecha.setDia(fecha.getDia() + 1);
+                }
+                return fecha;
+            default:
+                throw new IllegalArgumentException("Fecha no válida.");
+        }
     }
 
     public Fecha siguienteMes(Fecha fecha){
-        if(fecha.getMes == 12){
+        if(fecha.getMes() == 12){
             fecha.setMes(1);
         }else{
             fecha.setMes(fecha.getMes() + 1);
         }
+        return fecha;
+    }
+
+    public String toString(){
+        return dia + "-" + mes + "-" + anio;
     }
 
     // Getters y Setters.
